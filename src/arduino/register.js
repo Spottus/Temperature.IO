3ad087bbd5148b787e4080b7aa63a53d233a4728
Luca Brandimarti <lucabrandimarti.00@gmail.com>
2020-05-25 20:05:50 +0200

    [UPDATE] register start method

--- src/arduino/register.js
+++ src/arduino/register.js
@@ -1,57 +1,57 @@
-const { Board, Thermometer } = require("johnny-five");
-const fs  = require('fs');
-const path = require('path');
-
+const { Board, Thermometer } = require('johnny-five')
+const fs = require('fs')
+const path = require('path')
 
 const board = new Board()
 
+function start() {
+  board.on('ready', () => {
+    const thermometer = new Thermometer({
+      controller: 'TMP36',
+      pin: 'A0',
+    })
 
-board.on("ready", () => {
+    thermometer.on('change', () => {
+      const ts = Date.now()
+      const date = new Date(ts)
+      let day = date.getDate()
+      let month = date.getMonth() + 1
+      let hours = date.getUTCHours() + 2
+      let year = date.getFullYear()
+      const { celsius } = thermometer
+      let record = day + ',' + month + ',' + year + ',' + hours + ',' + celsius
+      save(day,month,hours,year,celsius,record);
+    })
+  })
+}
 
-  const thermometer = new Thermometer({
-    controller: "TMP36",
-    pin: "A0"
-  });  
+function save(day,month,hours,year,celsius,record) {
 
-  thermometer.on("change", () => {
-    
-    const ts = Date.now();
-    const date = new Date(ts);
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let hours = date.getUTCHours() +2;
-    let year = date.getFullYear()
-    const {celsius} = thermometer;
-    let record = day + ',' + month + ',' + year + ',' + hours + ',' + celsius;
+  console.log('Thermometer')
+  console.log('day: ', day)
+  console.log('month: ', month)
+  console.log('year: ', year)
+  console.log('hours: ', hours)
+  console.log('celsius: ', celsius)
+  console.log('--------------------------------------')
 
-    console.log("Thermometer");
-    console.log("  day            : ", day);
-    console.log("  month          : ", month);
-    console.log("  year           : ", year)
-    console.log("  hours          : ", hours);
-    console.log("  celsius        : ", celsius);
-    console.log("--------------------------------------");
-    
-    var fs = require('fs');
-    var logStream = fs.createWriteStream(path.resolve('./src/database/log.csv'), {flags: 'a'});
-    logStream.write(record);
-    logStream.end('\n');
-    sleep(1000);
-    
-  });
+  var fs = require('fs')
+  var logStream = fs.createWriteStream(path.resolve('./src/database/log.csv'), {
+    flags: 'a',
+  })
+  logStream.write(record)
+  logStream.end('\n')
+  sleep(60000);
+}
 
-
-  function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-
-});
+function sleep(milliseconds) {
+  const date = Date.now()
+  let currentDate = null
+  do {
+    currentDate = Date.now()
+  } while (currentDate - date < milliseconds)
+}
 
 
-module.exports  = {board};
-
+module.exports = { board, start }
 
